b87a682f5fc19c173163cc77da15f95e901303d9
Angel1Ionita <ionita_angel_24@yahoo.com>
2023-03-09 02:34:07 +0200

    enable cors

--- src/main/java/ro/bitbrawlers/parking/controller/ParkingController.java
+++ src/main/java/ro/bitbrawlers/parking/controller/ParkingController.java
@@ -2,6 +2,7 @@ package ro.bitbrawlers.parking.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,6 +13,7 @@ import ro.bitbrawlers.parking.service.ParkingSpotService;
 
 import java.util.List;
 
+@CrossOrigin
 @Controller
 public class ParkingController {
 
