6986bb7b8a2c3d649d074eee2cc04c92e29b7f12
Andrei-Lixi <119952828+Andrei-Lixi@users.noreply.github.com>
2023-03-15 19:05:46 +0000

    added add-reservation endpoint

--- src/main/java/ro/bitbrawlers/parking/data/ParkingSpotRepository.java
+++ src/main/java/ro/bitbrawlers/parking/data/ParkingSpotRepository.java
@@ -23,11 +23,29 @@ public interface ParkingSpotRepository extends JpaRepository<ParkingSpot, Intege
     @Modifying
     @Query(value="update parkingSpot\n" +
             "\tset reservationId=null where reservationId = \n" +
-            "\t(select id  from reservation where licensePlate = ?1);", nativeQuery = true)
+            "\t(select id  from reservation where licensePlate = ?1)", nativeQuery = true)
     int emptyParkingSpot (String licensePlate);
 
     @Transactional
     @Modifying
     @Query(value="delete from reservation where licensePlate= ?1", nativeQuery=true)
     void deleteReservation (String licensePlate);
+
+    //@Query(value="select count(id) from parkingSpot where parkingSpot.reservationID is null",nativeQuery=true)
+    
+    
+   
+    @Transactional
+    @Modifying
+    @Query(value="insert into reservation(firstName, lastName, licensePlate) values(?1,?2,?3)",nativeQuery=true)
+    void addReservation(String firstName, String lastName,String licensePlate);
+    
+    @Transactional
+    @Modifying
+    @Query(value="update parkingSpotset reservationID = (select reservation.id from reservation where licensePlate=?1)where parkingSpot.id = (select id from parkingSpot where parkingSpot.reservationID is null limit 1)",nativeQuery=true)
+    void updateParkingSpotRow(String licensePlate);
+
+
+
+
 }
